07be86dcca53e9e3e4280fe12a66397ecd528710
FranciscoVAmaral <132096888+FranciscoVAmaral@users.noreply.github.com>
2024-03-01 20:22:46 -0100

    Add files via upload

---
+++ NpcScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+    //Player Characteristics
+
+    public int health = 5;
+   
+    public int level = 1;
+   
+    public float speed = 1.2f;
+}   Vector3 newPosition
+{
+    // Start is called before the first frame update
+    void Start()
+{
+      Transform newPosition
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+            newPosition.z +=
+        speed * Time.deltaTime;
+
+    }
+}
